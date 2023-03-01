567337f60002e3849b43f3cad5b221cd16123326
yacinebettir <bettiryassine@gmail.com>
2023-03-01 18:04:06 +0100

    My 1st one

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a= int(input('Enter ur Value :'))
+b=int(input("the power:"))
+s=1
+i=0
+while i<b:
+    s*=a
+    i+=1
+print(f'The power of :{a} \nBy :{b} is =={s}')
